e4a1e2577f5912f4c127f1048ac590432f76d5da
StEn <github@steven-england.info>
2024-01-01 20:37:30 +0100

    refactor: cut off log prefix

--- src/logger_lib/IobAdapterLogger.ts
+++ src/logger_lib/IobAdapterLogger.ts
@@ -4,16 +4,16 @@ import { LoggerInterface } from './LoggerInterface';
 export class IobAdapterLogger implements LoggerInterface {
   private _adapter: utils.AdapterInstance;
   public debug(message: string): void {
-    this._adapter.log.debug('# JSM: ' + message);
+    this._adapter.log.debug(message);
   }
   public info(message: string): void {
-    this._adapter.log.info('# JSM: ' + message);
+    this._adapter.log.info(message);
   }
   public warn(message: string): void {
-    this._adapter.log.warn('# JSM: ' + message);
+    this._adapter.log.warn(message);
   }
   public error(message: string): void {
-    this._adapter.log.error('# JSM: ' + message);
+    this._adapter.log.error(message);
   }
 
   public constructor(adapter: utils.AdapterInstance) {
