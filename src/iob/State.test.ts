1a5664acab5f8dacec6b655a82fc306a9c319751
StEn <github@steven-england.info>
2024-01-08 19:53:01 +0100

    test: correct test to use utc

--- src/iob/State.test.ts
+++ src/iob/State.test.ts
@@ -1,28 +1,32 @@
 import { expect } from 'chai';
+import { nameof } from '../utils/NameOf';
 import { State, StateValueType } from './State';
 
 describe('State', () => {
-  describe('setTimeStamp', () => {
-    (
-      [
-        [1697792400, '2023-10-20T11:00:00'],
-        [1697792400, '2023-10-20 11:00:00'],
-        [NaN, '2023-19-99 99:99:99'],
-        [NaN, false],
-        [1111, 1111],
-      ] as Array<[number, StateValueType]>
-    ).forEach(([expected, input]) => {
-      it(`should return ${expected} with state timestamp of ${input} given`, () => {
-        // GIVEN
-        const state = new State({
-          val: input,
-        });
-        // WHEN
-        state.setTimeStamp(input);
+  describe(
+    nameof<State>((s) => s.setTimeStamp),
+    () => {
+      (
+        [
+          [1697799600, '2023-10-20T11:00:00Z'],
+          [1697799600, '2023-10-20 11:00:00Z'],
+          [NaN, '2023-19-99 99:99:99'],
+          [NaN, false],
+          [1111, 1111],
+        ] as Array<[number, StateValueType]>
+      ).forEach(([expected, input]) => {
+        it(`should return ${expected} with state timestamp of ${input} given`, () => {
+          // GIVEN
+          const state = new State({
+            val: input,
+          });
+          // WHEN
+          state.setTimeStamp(input);
 
-        // THEN
-        expect(state.ts).deep.equal(expected);
+          // THEN
+          expect(state.ts).deep.equal(expected);
+        });
       });
-    });
-  });
+    },
+  );
 });
